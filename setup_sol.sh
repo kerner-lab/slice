d7f267618f9cb324663b46231e2bd6eb62ed7560
Manthan-Sol <mnthn.ai@gmail.com>
2023-04-29 16:55:03 -0700

    WIP

--- setup_sol.sh
+++ setup_sol.sh
@@ -1,3 +1,7 @@
+# Load modules
 module load mamba/latest
-source activate slice
 module load cuda-11.7.0-gcc-11.2.0
+
+# Check if slice exists, if not create it and activate it
+if conda info --envs | grep -q slice; then echo "slice already exists"; elsemamba env create -f slice_sol.yaml -y; fi
+source activate slice
