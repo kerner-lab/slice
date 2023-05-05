f93216b85ac15fe496f1b991f716e85d41edefe3
Manthan-Sol <mnthn.ai@gmail.com>
2023-05-05 12:14:58 -0700

    Chore: Sampled Data

--- data/extract.py
+++ data/extract.py
@@ -2,14 +2,3 @@ from pyunpack import Archive
 import geopandas as gpd
 
 Archive('france_data.7z').extractall("./france/")
-
-path = "./france/PARCELLES_GRAPHIQUES.gpkg"
-
-# Load the data
-data = gpd.read_file(path)
-
-# Split the data into 30 chunks and save them
-# to the data/france/ directory
-data_split = np.array_split(data, 30)
-for i in range(len(data_split)):
-    data_split[i].to_file(f"data/france/data_{i}.gpkg", driver="GPKG")
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -7,21 +7,7 @@ import os
 import sys
 import geopandas as gpd
 
-path = "data/france/data_10.gpkg"
+path = "data/france/sampled.gpkg"
 
 # Load the data
-data = gpd.read_file(path)
-data = data.to_crs(epsg=4326)
-
-roi = [
-[[-0.3188344314545766, 49.842186963435914],
- [2.0768077353481242, 49.842186963435914],
- [2.0768077353481242, 42.347232877570896],
- [-0.3188344314545766, 42.347232877570896]]
-]
-
-
-d1 = data[data.bounds.maxy < 46]
-
-# Save d1
-d1.to_file("data/france/d1.gpkg", driver="GPKG")
+raw_data = gpd.read_file(path)
