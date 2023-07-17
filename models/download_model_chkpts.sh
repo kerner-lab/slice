9b40fb97803816f6a7c6252d62690b5acd3cd5d9
Aninda Ghosh <aghosh57@asu.edu>
2023-07-17 16:14:00 -0700

    WIP: Added MaskRCNN Training Repo and SAT_SAM Repo

--- models/download_model_chkpts.sh
+++
@@ -1,4 +0,0 @@
-mkdir ./segment-anything/chkpts
-wget -O ./segment-anything/chkpts/sam_vit_h.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
-wget -O ./segment-anything/chkpts/sam_vit_l.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth
-wget -O ./segment-anything/chkpts/sam_vit_b.pth https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
